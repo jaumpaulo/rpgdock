d74ac6578c894d85c3c92657b122bd465dde80da
João Paulo <joao.filho488@aluno.ce.gov.br>
2026-09-23 22:38:56 -0300

    Add files via upload

--- Ficha.js
+++ Ficha.js
@@ -40,4 +40,4 @@ class Ficha {
     }
 }
 
-module.exports = Ficha;
+export default Ficha;
--- app.js
+++ app.js
@@ -1,35 +1,74 @@
-const prompt = require("prompt-sync")();
-const Ficha = require("./Ficha");
+import Ficha from "./Ficha.js";
+import { input, select } from "@inquirer/prompts";
 
 let fichas = [];
 
-function criarFicha() {
+async function criarFicha() {
     console.log("\n==============================");
     console.log("         CRIAR FICHA");
     console.log("==============================");
 
-    let nome = prompt("Nome: ");
-    let raca = prompt("Raça: ");
-    let classe = prompt("Classe: ");
-    let nivel = parseInt(prompt("Nível: "));
+    let nome = await input({message: "Nome: "});
+    let raca = await select({
+        message: "Escolha sua raça",
+        choices:[
+            {name:"Humano", value:"Humano"},
+            {name:"Anão", value:"Anão"},
+            {name:"Dahllan", value:"Dahllan"},
+            {name:"Elfo", value:"Elfo"},
+            {name:"Goblin", value:"Goblin"},
+            {name:"Lefou", value:"Lefou"},
+            {name:"Minotauro", value:"Minotauro"},
+            {name:"Qareen", value:"Qareen"},
+            {name:"Golem", value:"Golem"},
+            {name:"Hynne", value:"Hynne"},
+            {name:"Kliren", value:"Kliren"},
+            {name:"Medusa", value:"Medusa"},
+            {name:"Osteon", value:"Osteon"},
+            {name:"Sereia/Tritão", value:"Sereia/Tritão"},
+            {name:"Sílfide", value:"Sílfide"},
+            {name:"Suraggel", value:"Suraggel"},
+            {name:"Trog", value:"Trog"}
+        ]
+    });
+    let classe = await select({
+        message: "Escolha sua classe",
+        choices:[
+            {name:"Arcanista", value:"Arcanista"},
+            {name:"Bárbaro", value:"Bárbaro"},
+            {name:"Bardo", value:"Bardo"},
+            {name:"Bucaneiro", value:"Bucaneiro"},
+            {name:"Caçador", value:"Caçador"},
+            {name:"Cavaleiro", value:"Cavaleiro"},
+            {name:"Clérigo", value:"Clérigo"},
+            {name:"Druida", value:"Druida"},
+            {name:"Guerreiro", value:"Guerreiro"},
+            {name:"Inventor", value:"Inventor"},
+            {name:"Ladino", value:"Ladino"},
+            {name:"Lutador", value:"Lutador"},
+            {name:"Nobre", value:"Nobre"},
+            {name:"Paladino", value:"Paladino"}
+        ]
+    });
+    let nivel = parseInt(await input({ message:"Nível: " }));
 
     let ficha = new Ficha(nome, raca, classe, nivel);
 
     console.log("\n--- ATRIBUTOS ---");
 
-    ficha.forca = parseInt(prompt("Força: "));
-    ficha.destreza = parseInt(prompt("Destreza: "));
-    ficha.constituicao = parseInt(prompt("Constituição: "));
-    ficha.inteligencia = parseInt(prompt("Inteligência: "));
-    ficha.sabedoria = parseInt(prompt("Sabedoria: "));
-    ficha.carisma = parseInt(prompt("Carisma: "));
+    ficha.forca = parseInt(await input({ message: "Força: " }));
+    ficha.destreza = parseInt(await input({ message: "Destreza: " }));
+    ficha.constituicao = parseInt(await input({ message: "Constituição: " }));
+    ficha.inteligencia = parseInt(await input({ message: "Inteligência: " }));
+    ficha.sabedoria = parseInt(await input({ message: "Sabedoria: " }));
+    ficha.carisma = parseInt(await input({ message: "Carisma: " }));
 
     fichas.push(ficha);
 
     console.log("\nFicha criada com sucesso!");
 }
 
-function verFichas() {
+async function verFichas() {
     console.log("\n==============================");
     console.log("          SUAS FICHAS");
     console.log("==============================");
@@ -46,7 +85,7 @@ function verFichas() {
         console.log(`   Nível: ${ficha.nivel}`);
     })
 
-    let escolha = parseInt(prompt("\nDigite o número da ficha para ver: "));
+    let escolha = parseInt(await input({message: "\nDigite o número da ficha para ver: " }));
 
     if (escolha >= 1 && escolha <= fichas.length) {
         fichas[escolha - 1].mostrarFicha();
@@ -55,22 +94,24 @@ function verFichas() {
     }
 }
 
-function menu() {
+async function menu() {
     while (true) {
         console.log("\n==============================");
         console.log("           FICHA RPG");
         console.log("==============================");
-        console.log("1 - Criar ficha");
-        console.log("2 - Ver fichas");
-        console.log("3 - Encerrar programa");
-        console.log("==============================");
-
-        let opcao = prompt("Escolha uma opção: ");
+        const opcao = await select({
+            message: "Escolha uma opção: ",
+            choices:[
+                {name: "1 - Criar ficha", value: 1},
+                {name: "2 - Ver fichas", value: 2},
+                {name: "3 - Encerrar programa", value: 3}
+            ]
+        });
 
         if (opcao == 1) {
-            criarFicha();
+            await criarFicha();
         } else if (opcao == 2) {
-            verFichas();
+            await verFichas();
         } else if (opcao == 3) {
             console.log("\nPrograma encerrado!");
             break;
